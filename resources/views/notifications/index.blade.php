c5cec723187bf2ad49c49e8f92fb4a300edab396
ahmdsydysf <a.sayed.xc@gmail.com>
2025-04-20 19:29:18 +0400

    notification

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -19,67 +19,105 @@
         </div>
     </div>
 
-    <!-- Notifications List -->
+    <!-- Notifications Table -->
     <section class="hk-sec-wrapper">
         <div class="row">
             <div class="col-sm">
-                <div class="card-group">
-                    @forelse($notifications as $notification)
-                        <div class="card mb-3 {{ $notification->read_at ? 'bg-light' : 'border-left-info' }}"
-                             style="{{ $notification->read_at ? '' : 'border-left: 4px solid #2c95ff;' }}">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between align-items-center mb-2">
-                                    <h5 class="card-title mb-0">
-                                        {{ ucfirst(str_replace('_', ' ', $notification->type)) }}
-                                    </h5>
-                                    <small class="text-muted">{{ $notification->created_at->diffForHumans() }}</small>
-                                </div>
+                <div class="table-wrap">
+                    <div class="table-responsive">
+                        <table class="table table-hover table-bordered mb-0">
+                            <thead class="thead-primary">
+                                <tr>
+                                    <th width="5%">#</th>
+                                    <th width="20%">Type</th>
+                                    <th width="35%">Message</th>
+                                    <th width="15%">Time</th>
+                                    <th width="10%">Status</th>
+                                    <th width="15%">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse($notifications as $key => $notification)
+                                    <tr class="{{ $notification->read_at ? 'bg-light' : 'bg-light-info' }}">
+                                        <td>{{ $notifications->firstItem() + $key }}</td>
+                                        <td>
+                                            <span class="font-weight-600">
+                                                {{ ucfirst(str_replace('_', ' ', $notification->type)) }}
+                                            </span>
+                                        </td>
+                                        <td>
+                                            @if(isset($notification->data['message']))
+                                                {{ $notification->data['message'] }}
+                                            @else
+                                                {{ json_encode($notification->data) }}
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <span class="text-muted">{{ $notification->created_at->diffForHumans() }}</span>
+                                            <br>
+                                            <small class="text-muted">{{ $notification->created_at->format('M d, Y g:i A') }}</small>
+                                        </td>
+                                        <td>
+                                            @if($notification->read_at)
+                                                <span class="badge badge-light">Read</span>
+                                            @else
+                                                <span class="badge badge-info">Unread</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <div class="d-flex">
+                                                @if(isset($notification->data['url']))
+                                                    <a href="{{ $notification->data['url'] }}" class="btn btn-primary btn-sm mr-10">
+                                                        <i class="fas fa-eye"></i> View
+                                                    </a>
+                                                @endif
 
-                                <p class="card-text">
-                                    @if(isset($notification->data['message']))
-                                        {{ $notification->data['message'] }}
-                                    @else
-                                        {{ json_encode($notification->data) }}
-                                    @endif
-                                </p>
+                                                @if($notification->read_at)
+                                                    <form action="{{ route('notifications.mark-as-unread', $notification->id) }}" method="POST">
+                                                        @csrf
+                                                        <button type="submit" class="btn btn-outline-secondary btn-sm">
+                                                            <i class="fas fa-envelope"></i>
+                                                        </button>
+                                                    </form>
+                                                @else
+                                                    <form action="{{ route('notifications.mark-as-read', $notification->id) }}" method="POST">
+                                                        @csrf
+                                                        <button type="submit" class="btn btn-outline-success btn-sm">
+                                                            <i class="fas fa-envelope-open"></i>
+                                                        </button>
+                                                    </form>
+                                                @endif
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="6" class="text-center">No notifications found.</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
 
-                                @if(isset($notification->data['url']))
-                                    <a href="{{ $notification->data['url'] }}" class="btn btn-outline-primary btn-sm">
-                                        View Details
-                                    </a>
-                                @endif
-
-                                <div class="d-flex mt-3">
-                                    @if($notification->read_at)
-                                        <form action="{{ route('notifications.mark-as-unread', $notification->id) }}" method="POST">
-                                            @csrf
-                                            <button type="submit" class="btn btn-outline-secondary btn-sm">
-                                                Mark as Unread
-                                            </button>
-                                        </form>
-                                    @else
-                                        <form action="{{ route('notifications.mark-as-read', $notification->id) }}" method="POST">
-                                            @csrf
-                                            <button type="submit" class="btn btn-outline-success btn-sm">
-                                                Mark as Read
-                                            </button>
-                                        </form>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                    @empty
-                        <div class="col-12 text-center">
-                            <p>No notifications found.</p>
-                        </div>
-                    @endforelse
-                </div>
-
-                <div class="d-flex justify-content-end mt-20">
-                    {{ $notifications->links() }}
+                    <div class="d-flex justify-content-end mt-20">
+                        {{ $notifications->links() }}
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </div>
+
+<style>
+    .thead-primary th {
+        background-color: #0f5874 !important;
+        color: white;
+    }
+    .bg-light-info {
+        background-color: #e8f4ff;
+    }
+    .mr-10 {
+        margin-right: 10px;
+    }
+</style>
 @endsection
